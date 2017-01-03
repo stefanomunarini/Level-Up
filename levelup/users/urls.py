249b1623fac4029cec99afcb1ba5f90a099d0121
santala <simo.a.santala@aalto.fi>
2017-01-03 15:18:53 +0200

    Translations partially added. Changing language won't work

--- levelup/users/urls.py
+++ levelup/users/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 import django
-from django.conf.urls import url
+from django.conf.urls import url, include, i18n
 from django.contrib.auth.views import password_reset, logout
 from django.urls import reverse_lazy
 
