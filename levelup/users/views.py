983d1150a7d5899a699162552cfe80689c94c67b
Stefano <ssteee@gmail.com>
2017-01-12 00:08:18 +0200

    Save User only if UserProfile can be saved as well

--- levelup/users/views.py
+++ levelup/users/views.py
@@ -1,34 +1,28 @@
-from django.contrib import auth, messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import User, Group
-from django.forms import model_to_dict
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
-from django.urls import reverse, reverse_lazy
+from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView, UpdateView
-from django.views.generic.edit import ProcessFormView
-from django.utils.translation import ugettext_lazy as _
-
 from games.models import Game
-from users.models import UserProfile
 from users.forms import (
     SignupPlayerForm, SignupDeveloperForm,
-    UserUpdateModelForm, UserProfileUpdateModelForm, UserProfileUpdateModelFormset
+    UserUpdateModelForm, UserProfileUpdateModelFormset
 )
+from users.models import UserProfile
 
 
 # User Signup
 
 class AbstractSignupView(FormView):
     """
-        This acts as a generic SignupView that is used by SignupPlayerView and SignupDeveloperView
-        """
+    This acts as a generic SignupView that is used by SignupPlayerView and SignupDeveloperView
+    """
     template_name = 'signup.html'
     success_url = reverse_lazy('profile:user-profile')
 
     def get(self, request, *args, **kwargs):
-        # Don’t allow signups if the user is logged in
+        # Don’t allow signups if the user is already logged in
         if request.user.is_authenticated:
             return HttpResponseRedirect(reverse_lazy('home'))
         return super(AbstractSignupView, self).get(self, request, *args, **kwargs)
@@ -81,7 +75,6 @@ class UserProfileDetailView(LoginRequiredMixin, UserProfileMixin, TemplateView):
 class UserProfileUpdateView(LoginRequiredMixin, UserProfileMixin, UpdateView):
     form_class = UserUpdateModelForm
     login_url = reverse_lazy('login')
-    model = User
     success_url = reverse_lazy('profile:user-profile')
     template_name = 'user_profile_update_view.html'
 
@@ -104,17 +97,19 @@ class UserProfileUpdateView(LoginRequiredMixin, UserProfileMixin, UpdateView):
             request.FILES
         )
         if user_form.is_valid():
-            user_form.save()
-            user_profile_form.save(commit=False)
-            """
-            There is always one and only one formset (one user_profile for every user)
-            hence we only and always save the first formset
-            """
-            user_profile_form[0].instance.user = self.get_object()
-            user_profile_form[0].save()
-            return super(UserProfileUpdateView, self).form_valid(form=user_form)
-        else:
-            return render(request, self.template_name, {'user_form': user_form, 'user_profile_form': user_profile_form})
+            user_form.save(commit=False)
+            if user_profile_form.is_valid():
+                user_profile_form.save(commit=False)
+                """
+                There is always one and only one formset (one user_profile for every user)
+                hence we only and always save the first formset
+                """
+                user_profile_form[0].instance.user = self.get_object()
+                user_profile_form[0].save()
+                user_form.save()
+                return super(UserProfileUpdateView, self).form_valid(form=user_form)
+        return render(request, self.template_name, {'user_form': user_form, 'user_profile_form': user_profile_form})
+
 
 
 """
