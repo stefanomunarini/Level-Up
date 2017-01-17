565a1ff3c62fb8d0d690c5a1e58c7756540225c2
Stefano <ssteee@gmail.com>
2017-01-17 22:32:09 +0200

    Limit the number of games to display in homepage

--- levelup/services.py
+++ levelup/services.py
@@ -4,6 +4,8 @@ from django.db.models import Count
 
 from games.models import Game
 
+GAMES_TO_DISPLAY = 10
+
 
 def get_homepage_games():
     games = Game.objects.all()
@@ -18,18 +20,18 @@ def get_homepage_games():
 def get_best_sellers(games):
     return games.annotate(downloads=Count('transactions')) \
         .order_by('-downloads') \
-        .filter(downloads__gt=0)
+        .filter(downloads__gt=0)[:GAMES_TO_DISPLAY]
 
 
 def get_trending_this_week(games, today):
     one_week_ago = today - timedelta(7)
     return games.filter(transactions__datetime__gte=one_week_ago) \
-        .annotate(downloads=Count('transactions')) \
-        .order_by('-downloads')
+               .annotate(downloads=Count('transactions')) \
+               .order_by('-downloads')[:GAMES_TO_DISPLAY]
 
 
 def get_trending_this_month(games, today):
     one_month_ago = today - timedelta(31)
     return games.filter(transactions__datetime__gte=one_month_ago) \
-        .annotate(downloads=Count('transactions')) \
-        .order_by('-downloads')
+               .annotate(downloads=Count('transactions')) \
+               .order_by('-downloads')[:GAMES_TO_DISPLAY]
