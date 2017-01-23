ad36a053acdf8edc62657f32fc12a7b6a929680c
Stefano <ssteee@gmail.com>
2017-01-23 17:36:07 +0200

    Secure API BaseView

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.utils.translation import ugettext_lazy as _
 from django.views import View
 
 from api import services
@@ -7,21 +8,29 @@ from users.models import ApiToken
 
 class ApiBaseView(View):
 
+    ERROR_MESSAGE = _('error')
+    NO_TOKEN_MESSAGE = _('Unauthorized! You need to provide a valid Bearer token to use the APIs.')
+    INVALID_TOKEN_MESSAGE = _('Unauthorized! The token you are trying to use is invalid. Format should be: Bearer TOKEN')
+
     def dispatch(self, request, *args, **kwargs):
         token = request.META.get('HTTP_TOKEN')
-        bearer = token.split(' ')[0]
+        if not token:
+            return JsonResponse(data={self.ERROR_MESSAGE: self.NO_TOKEN_MESSAGE}, status=401)
+        if not len(token.split(' ')) == 2 or 'Bearer' not in token:
+            return JsonResponse(data={self.ERROR_MESSAGE: self.INVALID_TOKEN_MESSAGE}, status=401)
         token_value = token.split(' ')[1]
         request_origin = self.request.is_secure() and 'https://' or 'http://' + self.request.META['HTTP_HOST']
         self.api_toke_obj = ApiToken.objects.filter(token=token_value, website_url=request_origin)
-        if not bearer == 'Bearer' and not self.api_toke_obj.exists():
+        if not self.api_toke_obj.exists():
             return JsonResponse(data={'error': 'Unauthorized'}, status=401)
         return super(ApiBaseView, self).dispatch(request, *args, **kwargs)
-        # return JsonResponse(data={'status': 'Yes'}, status=200)
 
 
 class MyDevelopedGames(ApiBaseView):
     def dispatch(self, request, *args, **kwargs):
-        super(MyDevelopedGames, self).dispatch(request, *args, **kwargs)
+        dispatcher = super(MyDevelopedGames, self).dispatch(request, *args, **kwargs)
+        if dispatcher.status_code != 200:
+            return dispatcher
         response = {
             'games': services.get_developed_games(self.api_toke_obj)
         }
