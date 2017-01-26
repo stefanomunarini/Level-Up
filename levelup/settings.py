2cefde932d2e7fd1c3cb87c951d31e4a7fa320d6
Stefano <ssteee@gmail.com>
2017-01-26 11:34:51 +0200

    Update whitenoise setup

--- levelup/settings.py
+++ levelup/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
