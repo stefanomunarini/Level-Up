e278a3251282a420d68f358b0e8c36f7a1f6e12d
Phu Pham <anphu.1225@gmail.com>
2017-01-29 15:24:33 +0200

    add twitter authentication and remove uploading social account avatar

--- levelup/settings.py
+++ levelup/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.twitter',
 ]
 
 MIDDLEWARE = [
